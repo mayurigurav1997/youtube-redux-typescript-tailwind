5c825a7996fff59b019f601f473e92ae51f8fb3d
Mayuri Gurav <mayurigurav1997@gmail.com>
2024-01-22 21:54:08 +0530

    Navbar started

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,6 +13,31 @@ export const Navbar = () => {
         <div>
           <GiHamburgerMenu />
         </div>
+        <Link to="/">
+          <div className="flex gap-1 item-center justify-center">
+            <BsYoutube className="text-3xl text-red-600" />
+            <span className="text-xl font-medium">YouTube</span>
+          </div>
+        </Link>
+      </div>
+      <div className="flex items-center justify-center gap-5">
+        <form action="">
+          <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0">
+            <div className="flex gap-4 items-center pr-5">
+              <div>
+                <AiOutlineSearch className="text-xl " />
+              </div>
+              <input
+                type="text"
+                className="w-96 bg-zinc-900 focus:outline-none border-none"
+              />
+              <AiOutlineClose className="text-xl cursor-pointer" />
+            </div>
+            <button className="h-10 w-16 flex items-center justify-center bg-zinc-800">
+              <AiOutlineSearch className="text-xl" />
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
